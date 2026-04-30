e4cdd4da69daeca285e142d6a381d93f92abf6a8
BN2R <146022660+bogiland@users.noreply.github.com>
2026-04-30 14:24:39 +0300

    Update IProductService.cs

--- BLL/Services/IProductService.cs
+++ BLL/Services/IProductService.cs
@@ -5,8 +5,8 @@ namespace TeslaStore.BLL.Services
     public interface IProductService
     {         
         IEnumerable<ProductDTO> GetAllProducts();
-        ProductDTO? GetProductById(int id);
-        ProductDTO CreateProduct(ProductDTO productDto);                    
+        ProductDTO? GetProductById(int id);          
+        ProductDTO CreateProduct(ProductDTO productDto);                              
         void UpdateProduct(int id, ProductDTO productDto);
         void DeleteProduct(int id);                    
     }
