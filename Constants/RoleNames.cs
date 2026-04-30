349cd8fea9ca683e8438d9951eb2511584379013
BN2R <146022660+bogiland@users.noreply.github.com>
2026-04-30 14:25:32 +0300

    Update RoleNames.cs

--- Constants/RoleNames.cs
+++ Constants/RoleNames.cs
@@ -2,6 +2,7 @@ namespace TeslaStore.Constants
 {
     public static class RoleNames
     {
+        
         public const string Admin = "Администратор";
         public const string User = "Пользователь";
         public const string Visitor = "Посетитель";
