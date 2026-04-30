b6e5ed7d2d4703f764da36ed4d7352d6d3700066
BN2R <146022660+bogiland@users.noreply.github.com>
2026-04-30 14:23:53 +0300

    Update ProductDTO.cs

--- BLL/DTOs/ProductDTO.cs
+++ BLL/DTOs/ProductDTO.cs
@@ -8,7 +8,7 @@ namespace TeslaStore.BLL.DTOs
         public string? Category { get; set; }  
         public string? Image { get; set; }
         public string? FallbackImage { get; set; }
-                                                  
+        
         // Дополнительные поля для демонстрации бизнес-логики (поведения и вычислений)  
         public string? FormattedPrice { get; set; }  
         public bool IsPremium { get; set; }   
