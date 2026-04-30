03d76955e39808129cf3349dbf6705e7247596f8
BN2R <146022660+bogiland@users.noreply.github.com>
2026-04-30 14:22:54 +0300

    Add FallbackImage property to ProductDTO

--- BLL/DTOs/ProductDTO.cs
+++ BLL/DTOs/ProductDTO.cs
@@ -7,7 +7,8 @@ namespace TeslaStore.BLL.DTOs
         public decimal Price { get; set; }
         public string? Category { get; set; }
         public string? Image { get; set; }
-
+        public string? FallbackImage { get; set; }
+        
         // Дополнительные поля для демонстрации бизнес-логики (поведения и вычислений)
         public string? FormattedPrice { get; set; }
         public bool IsPremium { get; set; }
