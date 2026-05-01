83de12731d84b126d92bf32b3fa3fd199f970d98
BN2R <146022660+bogiland@users.noreply.github.com>
2026-05-01 13:21:16 +0300

    Update ProductService.cs

--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -74,7 +74,7 @@ namespace TeslaStore.BLL.Services
 
             _repository.Update(existingEntity);
         }
-jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsn
+jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvd
 
 
         public void DeleteProduct(int id)
