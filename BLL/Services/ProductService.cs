6c3402a1a76a6d33063e5fe5bd35db8cb3d95d07
BN2R <146022660+bogiland@users.noreply.github.com>
2026-05-01 13:16:00 +0300

    Fix formatting issues in ProductService.cs

--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -80,7 +80,7 @@ jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsnjkdsvjknvnksjknvdsjbvdshui
 vfjnkfdsnkjsksvnjd
 
 
-nnsdnksdksdvkvd
+nnsdnksdksdvkv
         public void DeleteProduct(int id)
         {
             var existingEntity = _repository.GetById(id);
