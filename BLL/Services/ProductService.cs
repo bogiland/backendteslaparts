cc8e6dcd90faf4f1c8680d8e58c971b6e06f5445
BN2R <146022660+bogiland@users.noreply.github.com>
2026-05-01 13:18:45 +0300

    Clean up ProductService.cs by removing junk text
    Removed invalid characters from ProductService.cs.

--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -74,7 +74,7 @@ namespace TeslaStore.BLL.Services
 
             _repository.Update(existingEntity);
         }
-jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsnjkdsvjknvnksjknvdsjbvdshu
+jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsnjkdsvjknvnksjknvdsjbvds
 
         public void DeleteProduct(int id)
         {
