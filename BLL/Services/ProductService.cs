b0341b3be0a642dedb038a18936e03b4150f20cf
BN2R <146022660+bogiland@users.noreply.github.com>
2026-05-01 13:20:59 +0300

    Fix formatting issues in ProductService.cs

--- BLL/Services/ProductService.cs
+++ BLL/Services/ProductService.cs
@@ -74,7 +74,7 @@ namespace TeslaStore.BLL.Services
 
             _repository.Update(existingEntity);
         }
-jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsnjkds
+jksnjksdnjsdjnjdsjnksvdkjnsdnjksvdnjkvdskjnsvdsnjk
 
 
         public void DeleteProduct(int id)
